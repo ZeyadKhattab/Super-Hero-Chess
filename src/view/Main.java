a7e6033646696f3d7e7e1f6b68510d07da227654
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-05-21 21:56:41 +0200

    built static board

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,48 +1,53 @@
 package view;
 
+import java.io.FileInputStream;
+
 import javax.swing.JOptionPane;
 
 import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
 import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 import model.game.Game;
 import model.game.Player;
 
-public class Main extends Application{
+public class Main extends Application {
 
 	public static void main(String[] args) {
-//		String player1Name=JOptionPane.showInputDialog("Player 1 Name"),player2Name=JOptionPane.showInputDialog("Player 2 Name");
-//		Game game=new Game(new Player(player1Name),new Player(player2Name));
+
 		Application.launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("GridPane Experiment");
+		String player1Name = /* JOptionPane.showInputDialog("Player 1 Name") */"zoz",
+				player2Name = /* JOptionPane.showInputDialog("Player 2 Name") */"bro";
+		Game game = new Game(new Player(player1Name), new Player(player2Name));
 
-        Button button1 = new Button("Button 1");
-        Button button2 = new Button("Button 2");
-        Button button3 = new Button("Button 3");
-        Button button4 = new Button("Button 4");
-        Button button5 = new Button("Button 5");
-        Button button6 = new Button("Button 6");
+		GridPane gridPane = new GridPane();
+		for (int i = 0; i < game.getBoardHeight(); i++)
+			for (int j = 0; j < game.getBoardWidth(); j++) {
+				FileInputStream input = new FileInputStream("images/resize.jpg");
+				Image image = new Image(input);
+				ImageView imageView = new ImageView(image);
+				Button button = new Button("",imageView);
+				button.setId(""+(i * game.getBoardHeight() + j));
+				button.setOnAction(value -> {
+					System.out.println(((Button) value.getSource()).getId());
+				});
+				gridPane.add(button, j, i);
+			}
 
-        GridPane gridPane = new GridPane();
+		Scene scene = new Scene(gridPane, game.getBoardWidth() * 100, game.getBoardHeight() * 80);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 
-        gridPane.add(button1, 0, 0, 1, 1);
-        gridPane.add(button2, 1, 0, 1, 1);
-        gridPane.add(button3, 2, 0, 1, 1);
-        gridPane.add(button4, 0, 1, 1, 1);
-        gridPane.add(button5, 1, 1, 1, 1);
-        gridPane.add(button6, 2, 1, 1, 1);
-
-        Scene scene = new Scene(gridPane, 240, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-		
 	}
 
 }
